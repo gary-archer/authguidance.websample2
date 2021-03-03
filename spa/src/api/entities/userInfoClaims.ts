6edc11a64df4bff48e0699388ca15efac1e4d0ba
gary <gary@MACSTATION.local>
2021-03-03 18:50:06 +0000

    Updates

--- spa/src/api/entities/userInfoClaims.ts
+++ spa/src/api/entities/userInfoClaims.ts
@@ -1,8 +1,7 @@
 /*
- * User info claims can be returned to the UI or used if the API triggers sending of emails to users
+ * User info claims can be returned to the UI
  */
 export interface UserInfoClaims {
     givenName: string;
     familyName: string;
-    email: string;
 }
